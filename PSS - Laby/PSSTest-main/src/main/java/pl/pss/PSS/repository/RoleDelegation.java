d5c8d2e9325ff8bf956068ea2ff514e7f1ae7b1b
rogasiu <46726517+rogasiu@users.noreply.github.com>
2021-03-21 18:47:48 +0100

    Delete PSS - Laby/PSSTest-main directory

--- PSS - Laby/PSSTest-main/src/main/java/pl/pss/PSS/repository/RoleDelegation.java
+++
@@ -1,9 +0,0 @@
-package pl.pss.PSS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.pss.PSS.model.Role;
-
-@Repository
-public interface RoleDelegation extends JpaRepository<Role, Long> {
-}
